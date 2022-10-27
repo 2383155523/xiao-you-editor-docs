ae088407d694e55e7140da96d04a2b79633cc7fb
微若蜉蝣 <2383155523@qq.com>
2022-10-27 15:43:14 +0800

    build: build on 2022 10 27

---
+++ demo/.vitepress/dist/assets/guide_props_theme.md.51de402e.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.2f75be1e.js";
+const __pageData = '{"title":"Theme","description":"","frontmatter":{},"relativePath":"guide/props/theme.md","lastUpdated":1666451864005}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="theme" tabindex="-1">Theme <a class="header-anchor" href="#theme" aria-hidden="true">#</a></h1><p>\u7F16\u8F91\u5668\u4E3B\u9898\u6A21\u5F0F</p><ul><li><p><strong>\u7C7B\u578B\uFF1A</strong> <code>string</code></p></li><li><p><strong>\u53D6\u503C\u8303\u56F4\uFF1A</strong> <code>light | dark</code></p></li><li><p><strong>\u8BE6\u7EC6\u4FE1\u606F</strong></p></li></ul><p>\u7F16\u8F91\u5668\u4E3B\u9898\u6A21\u5F0F</p>', 4);
+const _hoisted_5 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_5);
+}
+var theme = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, theme as default };
