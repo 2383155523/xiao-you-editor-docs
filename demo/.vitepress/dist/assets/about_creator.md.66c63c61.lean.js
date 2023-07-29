fe42016c570c05da6144fab4b3d7eed7fb558ab6
微若蜉蝣 <2383155523@qq.com>
2023-07-29 10:28:08 +0800

    init

---
+++ demo/.vitepress/dist/assets/about_creator.md.66c63c61.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.8e6c7385.js";
+const __pageData = '{"title":"About Creator","description":"","frontmatter":{},"headers":[{"level":2,"title":"Info","slug":"info"},{"level":2,"title":"Contact","slug":"contact"}],"relativePath":"about/creator.md","lastUpdated":1690597329484}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 5);
+const _hoisted_6 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_6);
+}
+var creator = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, creator as default };
---
+++ demo/.vitepress/dist/assets/guide_props_templates.md.c1489838.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.8e6c7385.js";
+const __pageData = '{"title":"Templates","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u914D\u7F6E\u793A\u4F8B","slug":"\u914D\u7F6E\u793A\u4F8B"}],"relativePath":"guide/props/templates.md","lastUpdated":1690597329485}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 6);
+const _hoisted_7 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_7);
+}
+var templates = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, templates as default };
